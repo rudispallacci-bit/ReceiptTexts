2a41b6b1e3f72466cede7991c4117b45d6f9f4ec
Rudi Spallacci <rudispallacci@gmail.com>
2026-03-20 11:50:51 -0400

    Initial Commit

---
+++ ReceiptTexts/ReceiptTextsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReceiptTextsApp.swift
+//  ReceiptTexts
+//
+//  Created by Rudi Spallacci on 3/20/26.
+//
+
+import SwiftUI
+
+@main
+struct ReceiptTextsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
